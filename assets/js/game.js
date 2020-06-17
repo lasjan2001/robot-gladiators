793103450b37e3f588c10372b64513963927aa8f
LaShell Cole <65573897+lasjan2001@users.noreply.github.com>
2020-06-17 13:56:12 -0500

    looping completed

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,8 +16,6 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-
 var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
